8edbd7f07dc171ec5c6aaeb7d5efc3a2b4865696
Riku Murai <riku.murai15@imperial.ac.uk>
2019-09-30 18:07:19 +0100

    Add installation script for os-x

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform
-    echo "OS X"
+    # Vim settings
+    ln -sn $DIR/nvim ~/.config/nvim > /dev/null 2>&1
+    ln -sn $DIR/vim/.vimrc ~/.vimrc > /dev/null 2>&1
+    ln -sn $DIR/vim/.vim ~/.vim     > /dev/null 2>&1
+    # tmux settings
+    ln -sn $DIR/tmux/.tmux.conf ~/.tmux.conf > /dev/null 2>&1
+    # zsh settings
+    ln -sn $DIR/zsh/.zshrc ~/.zshrc   > /dev/null 2>&1
+    ln -sn $DIR/zsh/.zshenv ~/.zshenv > /dev/null 2>&1
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     configs=(rofi alacritty)
     for config in "${configs[@]}"; do
