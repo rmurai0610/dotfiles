6d65202b4d9a114fda716569428e7c748e722b99
Riku Murai <rmurai0610@gmail.com>
2022-01-22 02:48:42 +0000

    Fix

--- bin/dotfiles-install.sh
+++ bin/dotfiles-install.sh
@@ -9,33 +9,35 @@ if [ ! -d /home/riku/.local/bin ]; then
   mkdir /home/riku/.local/bin
 fi
 
-# Install curl if necessary
-which curl &> /dev/null || sudo apt install curl
-# Install tmux
-which tmux &> /dev/null || sudo apt install tmux
-# Install zsh
-which zsh &> /dev/null || sudo apt install zsh
-# Install Alacritty
-which alacritty &> /dev/null || sudo snap install alacritty --classic
-# Install Rofi
-which rofi &> /dev/null || sudo apt install rofi
-# Install Node
-which node &> /dev/null || {
-  curl -sL install-node.vercel.app/lts | sudo bash
-}
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # Install curl if necessary
+  which curl &> /dev/null || sudo apt install curl
+  # Install tmux
+  which tmux &> /dev/null || sudo apt install tmux
+  # Install zsh
+  which zsh &> /dev/null || sudo apt install zsh
+  # Install Alacritty
+  which alacritty &> /dev/null || sudo snap install alacritty --classic
+  # Install Rofi
+  which rofi &> /dev/null || sudo apt install rofi
+  # Install Node
+  which node &> /dev/null || {
+    curl -sL install-node.vercel.app/lts | sudo bash
+  }
+  if [ $(dpkg-query -W -f='${Status}' build-essential 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
+    sudo apt install build-essential
+  fi
+  # Install cmake
+  which cmake &> /dev/null || sudo snap install cmake --classic
 
-if [ $(dpkg-query -W -f='${Status}' build-essential 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  sudo apt install build-essential
+  which nvim &> /dev/null || {
+    curl -L -o nvim.appimage https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
+    mv nvim.appimage /home/riku/.local/bin/nvim
+    chmod u+x /home/riku/.local/bin/nvim
+  }
+
 fi
-# Install cmake
-which cmake &> /dev/null || sudo snap install cmake --classic
-
-which nvim &> /dev/null || {
-  curl -L -o nvim.appimage https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
-  mv nvim.appimage /home/riku/.local/bin/nvim
-  chmod u+x /home/riku/.local/bin/nvim
-}
 
 # install oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
