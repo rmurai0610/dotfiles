fe087ccd030679b5836f1a3f15057b2eb95b3394
Riku Murai <rmurai0610@gmail.com>
2024-04-06 23:29:49 +0100

    Add more nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,73 @@
+vim.g.mapleader = " "
+
+vim.opt.termguicolors = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.smartcase = true
+
+vim.opt.cursorline = true
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+vim.opt.undodir = vim.fn.expand("~/.vim/undo-dir")
+vim.opt.undofile = true
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-require('Comment').setup()
+require("everforest").load()
 
+vim.keymap.set('i', 'jk', '<ESC>')
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>')
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>')
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>')
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>')
+
+local function toggle_replace()
+	local api = require("nvim-tree.api")
+	api.node.open.replace_tree_buffer()
+	if api.tree.is_visible() then
+		api.tree.close()
+	else
+		api.tree.open({ current_window = true })
+	end
+end
+
+
+vim.keymap.set('n', '-', toggle_replace)
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+map('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<A-q>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<A-w>', '<Cmd>BufferClose<CR>', opts)
+
+map('n', '<leader>ff', ":Telescope frecency workspace=CWD<CR>", opts)
+map('n', '<leader>fg', ":Telescope live_grep<CR>", opts)
+map('n', '<leader>fb', ":Telescope buffers<CR>", opts)
+map('n', '<leader>fh', ":Telescope help_tags<CR>", opts)
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,198 @@
 return {
-	{'windwp/nvim-autopairs', event = "InsertEnter", config = true},
-	{ 'numToStr/Comment.nvim', lazy = false}
+	{ "folke/neodev.nvim",     opts = {} },
+	{ 'windwp/nvim-autopairs', event = "InsertEnter", config = true },
+	{
+		'numToStr/Comment.nvim',
+		lazy = false,
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("everforest").setup()
+		end,
+	},
+	{
+		'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+		config = function()
+			require("barbar").setup {
+				sidebar_filetypes = {
+					-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+					NvimTree = true,
+					Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+				},
+			}
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {
+				tab = { sync = { open = true, close = true } }
+			}
+		end,
+	},
+	{
+		"stevearc/dressing.nvim",
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.6',
+		dependencies = {
+			{ 'nvim-lua/plenary.nvim' },
+			{ "nvim-telescope/telescope-frecency.nvim" },
+		},
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			telescope.load_extension("frecency")
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+						}
+					}
+				},
+				extensions = {
+					frecency = {
+						show_scores = false,
+						show_unindexed = true,
+						show_filter_column = false,
+						ignore_patterns = { "*.git/*", "*/tmp/*", "*.pyc" },
+					},
+				},
+
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+			local capabilities = cmp_nvim_lsp.default_capabilities()
+			mason_lspconfig.setup_handlers({
+				-- default handler for installed servers
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+			})
+
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "hrsh7th/cmp-vsnip" },
+			{ "hrsh7th/vim-vsnip" },
+		},
+		config = function()
+			local cmp = require("cmp")
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0 and
+				    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") ==
+				    nil
+			end
+
+			local feedkey = function(key, mode)
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+			end
+
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					['<C-l>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm { select = true },
+					["<C-j>"] = cmp.mapping.confirm { select = true },
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif vim.fn["vsnip#available"](1) == 1 then
+							feedkey("<Plug>(vsnip-expand-or-jump)", "")
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+							feedkey("<Plug>(vsnip-jump-prev)", "")
+						end
+					end, { "i", "s" }),
+
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "vsnip" },
+				}, { { name = "buffer" }, { name = "path" } }),
+			})
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
+	{
+		'stevearc/conform.nvim',
+		config = function()
+			require("conform").setup({
+				format_on_save = {
+					lsp_fallback = true,
+					timeout_ms = 500,
+				}
+			})
+		end
+
+	}
 
 }
-
